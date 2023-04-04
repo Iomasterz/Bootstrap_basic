fbdcd87f59425f04e7e560c411126fb1feaa17f2
Iomasterz <iomaster.gg@gmail.com>
2023-04-05 00:10:18 +0700

    port

---
+++ nav-bar.php
@@ -0,0 +1,10 @@
+<nav id="colorlib-main-menu" role="navigation">
+    <ul>
+        <li class="colorlib-active"><a href="index.html">หน้าแรก</a></li>
+        <li><a href="photography.html">รูปภาพ</a></li>
+        <li><a href="travel.html">ท่องเที่ยว</a></li>
+        <li><a href="fashion.html">แฟชั่น</a></li>
+        <li><a href="about.html">เกี่ยวกับ</a></li>
+        <li><a href="contact.html">ติดต่อ</a></li>
+    </ul>
+</nav>
